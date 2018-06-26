47ad965a0dc589ad0e8a20d43eeb788d2b2fb07d
loriick <loricktravailleur@gmail.com>
2018-06-26 11:00:59 +0200

    front commit

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+let numbersBox = document.querySelectorAll(".numbers__datas-key");
+let time = 100;
+
+///NUMBERS
+numbersBox.forEach(numbers => {
+  numbs = numbers.textContent;
+  var demo = new CountUp(numbersBox, 0, parseInt(numbs), 0, 2.5);
+  if (!demo.error) {
+    demo.start();
+  } else {
+    console.error(demo.error);
+  }
+});
